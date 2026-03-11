7c9f420610032eba1c3438575fb659bb344e6535
H82M19\Student <Student>
2026-03-11 11:05:17 +0700

    fecth_syncs

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,51 @@
-import { useState, useEffect } from 'react' 
+import { useState, useEffect } from 'react';
 
 function App() {
-
   const [data, setData] = useState([]);
 
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    var url = "https://jsonplaceholder.typicode.com/users";
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => {
-        setData(data);
-        console.log(data);
-      })
-      .catch((error) => console.error("Lỗi rồi:", error));
-  }, []); 
+    const url = "https://jsonplaceholder.typicode.com/users";
+
+    async function fectchdata() {
+      try {
+        const res = await fetch(url);
+        const result = await res.json();
+        setData(result);
+      } catch (err) {
+  
+        setError(err.message);
+      } finally {
+   
+        setTimeout(() => {
+          setLoading(false);
+        }, 1000);
+      }
+    }
+
+
+    fectchdata(); 
+  }, []);
+
+
+ 
 
   return (
     <>
-      {data.map((item) => (
-      
-        <div key={item.id} >
-          <p> {item.name}</p>
-          <p> {item.email}</p>
-        </div>
-      ))}
+      {loading ? (
+        <p>Loading....</p>
+      ) : (
+        data.map((item) => (
+          <div key={item.id}>
+            <p><strong>{item.name}</strong></p>
+            <p>{item.email}</p>
+          </div>
+        ))
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
